7002cee337c6b41ad93861292857602b9a75e629
Nate Sales <nate@natesales.net>
2024-12-02 20:22:51 -0500

    feat: add outer TLS SNI proxy

--- go.mod
+++ go.mod
@@ -3,17 +3,27 @@ module github.com/tinfoilanalytics/nitro-attestation-shim
 go 1.23.2
 
 require (
+	github.com/caarlos0/env/v11 v11.2.2
+	github.com/go-acme/lego/v4 v4.20.4
 	github.com/hf/nsm v0.0.0-20220930140112-cd181bd646b9
 	github.com/mdlayher/vsock v1.2.1
+	github.com/miekg/dns v1.1.62
 	golang.org/x/crypto v0.28.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.2.0 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.4 // indirect
 	github.com/mdlayher/socket v0.4.1 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/tools v0.25.0 // indirect
 )
+
+replace github.com/mdlayher/vsock => github.com/natesales/vsock v0.0.0-20241202211744-c23986e4659a
--- server/main.go
+++ server/main.go
@@ -1,76 +1,177 @@
 package main
 
 import (
+	"bytes"
+	"crypto/tls"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
-	"sync"
+	"os"
+	"time"
 
 	"github.com/mdlayher/vsock"
+	"gopkg.in/yaml.v3"
+
+	"github.com/tinfoilanalytics/nitro-attestation-shim/pkg/util"
 )
+
+var version = "dev"
 
 var (
-	listenPort = flag.Int("port", 7000, "vsock port to listen on")
-	targetHost = flag.String("target", "127.0.0.1:8080", "target host:port to proxy to")
+	configFile = flag.String("config", "config.yml", "path to config file")
 )
 
-func proxyBetween(conn1, conn2 net.Conn) {
-	defer conn1.Close()
-	defer conn2.Close()
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		io.Copy(conn2, conn1)
-	}()
-
-	go func() {
-		defer wg.Done()
-		io.Copy(conn1, conn2)
-	}()
-
-	wg.Wait()
+type Config struct {
+	AllowedSNI []string `yaml:"allowed_sni"`
 }
 
-func handleConnection(conn net.Conn) {
+func loadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("reading config: %w", err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("parsing config: %w", err)
+	}
+
+	return &config, nil
+}
+
+func handleTLSConnection(conn net.Conn, allowedSNIs []string) {
 	defer conn.Close()
 	log.Printf("Accepted connection from %v", conn.RemoteAddr())
 
-	target, err := net.Dial("tcp", *targetHost)
+	buf := new(bytes.Buffer)
+	tr := io.TeeReader(conn, buf)
+
+	sni, err := peekSNI(tr)
 	if err != nil {
-		log.Printf("Failed to connect to target %s: %v", *targetHost, err)
+		log.Printf("Error reading SNI: %v", err)
+		return
+	}
+	log.Printf("SNI: %s", sni)
+
+	// Check if SNI is allowed
+	allowed := false
+	for _, allowedSNI := range allowedSNIs {
+		if sni == allowedSNI {
+			allowed = true
+			break
+		}
+	}
+	if !allowed {
+		log.Printf("SNI %s not in allowed list", sni)
 		return
 	}
 
-	log.Printf("Connected to target %s", *targetHost)
-	proxyBetween(conn, target)
+	ips, err := net.LookupIP(sni)
+	if err != nil {
+		log.Printf("Failed to resolve %s: %v", sni, err)
+		return
+	}
+	ip := ips[0].String()
+
+	tcpHost := net.JoinHostPort(ip, "443")
+	log.Printf("Dialing %s", tcpHost)
+	target, err := net.Dial("tcp", tcpHost)
+	if err != nil {
+		log.Printf("Failed to connect to target %s: %v", sni, err)
+		return
+	}
+	defer target.Close()
+
+	if _, err := io.Copy(target, buf); err != nil {
+		log.Printf("Error writing ClientHello: %v", err)
+		return
+	}
+
+	log.Printf("Connected to target %s (%s)", sni, ip)
+	util.CopyBetween(conn, target)
 	log.Printf("Connection closed")
 }
 
+func peekSNI(r io.Reader) (string, error) {
+	// Check if we have a TLS connection
+	peek := make([]byte, 4096)
+	_, err := io.ReadFull(r, peek[:5])
+	if err != nil {
+		return "", err
+	}
+	if peek[0] != 0x16 {
+		return "", fmt.Errorf("not a TLS handshake")
+	}
+
+	// Read the rest of the ClientHello
+	recordLen := int(peek[3])<<8 | int(peek[4])
+	if recordLen > 4096-5 {
+		recordLen = 4096 - 5
+	}
+	_, err = io.ReadFull(r, peek[5:recordLen+5])
+	if err != nil {
+		return "", err
+	}
+
+	var hello tls.ClientHelloInfo
+	err = tls.Server(readOnlyConn{bytes.NewReader(peek[:recordLen+5])}, &tls.Config{
+		GetConfigForClient: func(hi *tls.ClientHelloInfo) (*tls.Config, error) {
+			hello = *hi
+			return nil, nil
+		},
+	}).Handshake()
+
+	if hello.ServerName == "" {
+		return "", fmt.Errorf("no SNI found")
+	}
+
+	return hello.ServerName, nil
+}
+
+type readOnlyConn struct {
+	r io.Reader
+}
+
+func (c readOnlyConn) Read(p []byte) (int, error)         { return c.r.Read(p) }
+func (c readOnlyConn) Write(p []byte) (int, error)        { return 0, io.ErrClosedPipe }
+func (c readOnlyConn) Close() error                       { return nil }
+func (c readOnlyConn) LocalAddr() net.Addr                { return nil }
+func (c readOnlyConn) RemoteAddr() net.Addr               { return nil }
+func (c readOnlyConn) SetDeadline(t time.Time) error      { return nil }
+func (c readOnlyConn) SetReadDeadline(t time.Time) error  { return nil }
+func (c readOnlyConn) SetWriteDeadline(t time.Time) error { return nil }
+
 func main() {
+	log.SetPrefix("[tls-egress-shim-server] ")
+	log.Printf("version %s\n", version)
+
 	flag.Parse()
 
-	log.Printf("Starting vsock proxy server on port %d", *listenPort)
-	log.Printf("Proxying connections to %s", *targetHost)
-
-	listener, err := vsock.Listen(uint32(*listenPort), nil)
+	config, err := loadConfig(*configFile)
 	if err != nil {
-		log.Fatalf("Failed to listen on vsock port %d: %v", *listenPort, err)
+		log.Fatalf("Failed to load config: %v", err)
+	}
+	log.Printf("Loaded config with %d allowed SNIs", len(config.AllowedSNI))
+
+	var port uint32 = 7443
+
+	listener, err := vsock.Listen(port, nil)
+	if err != nil {
+		log.Fatalf("Failed to listen on vsock port %d: %v", port, err)
 	}
 	defer listener.Close()
 
-	log.Printf("Listening on vsock port %d", *listenPort)
+	log.Printf("Listening on vsock port %d", port)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Printf("Error accepting connection: %v", err)
+			log.Printf("Error accepting connection on port %d: %v", port, err)
 			continue
 		}
 
-		go handleConnection(conn)
+		go handleTLSConnection(conn, config.AllowedSNI)
 	}
 }
