1d3105f2484e9b4a635c2b164348672e553ca772
Nate Sales <nate@natesales.net>
2025-01-15 22:39:32 -0500

    fix: start proxy in startup startup script

--- examples/run.sh
+++ examples/run.sh
@@ -3,15 +3,17 @@ set -ex
 
 EXAMPLE=$1
 
+sudo pkill socat || true
 sudo nitro-cli terminate-enclave --all
 
 sudo nitro-cli run-enclave \
     --cpu-count 2 \
     --memory 2048 \
     --eif-path "$EXAMPLE/$EXAMPLE.eif" \
-    --enclave-cid 16 \
-    --debug-mode
+    --enclave-cid 16
 
 sudo nitro-cli describe-enclaves
 
-sudo nitro-cli console --enclave-name $EXAMPLE
+sudo socat tcp-listen:443,reuseaddr,fork "vsock-connect:$(nitro-cli describe-enclaves | jq -r '.[0].EnclaveCID'):443" &
+
+sudo nitro-cli console --enclave-name "$EXAMPLE"
