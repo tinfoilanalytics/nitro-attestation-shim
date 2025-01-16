4e2e98b8b2a4d39d27bb5b1cb8abb454bc1bbbea
Nate Sales <nate@natesales.net>
2025-01-16 01:12:47 -0500

    fix: debug mode and static enclave CID

--- examples/run.sh
+++ examples/run.sh
@@ -10,10 +10,11 @@ sudo nitro-cli run-enclave \
     --cpu-count 2 \
     --memory 2048 \
     --eif-path "$EXAMPLE/$EXAMPLE.eif" \
-    --enclave-cid 16
+    --enclave-cid 16 \
+    --debug-mode
 
 sudo nitro-cli describe-enclaves
 
-sudo socat tcp-listen:443,reuseaddr,fork "vsock-connect:$(nitro-cli describe-enclaves | jq -r '.[0].EnclaveCID'):443" &
+sudo socat tcp-listen:443,reuseaddr,fork "vsock-connect:16:443" &
 
 sudo nitro-cli console --enclave-name "$EXAMPLE"
